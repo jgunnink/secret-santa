6d19a89e6556a89c859cb1e912b8b727d30d00eb
JK Gunnink <jgunnink@gmail.com>
2016-07-18 22:22:54 +0800

    Adds model validations, factories and associated tests

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,5 +6,14 @@ class List < ActiveRecord::Base
   accepts_nested_attributes_for :santas, reject_if: :all_blank, allow_destroy: true
 
   validates :name, presence: true
+  validates :gift_day, presence: true
+
+  validate :gift_day_cannot_be_in_the_past, on: :create
+
+  def gift_day_cannot_be_in_the_past
+    if gift_day.present? && gift_day < Date.today
+      errors.add(:gift_day, "You can't set the gift day to be in the past")
+    end
+  end
 
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,5 +5,35 @@ RSpec.describe List do
   describe '@name' do
     it { should validate_presence_of(:name) }
   end
-  
+
+  describe '@gift_day' do
+    it { should validate_presence_of(:gift_day) }
+
+    context 'the gift day is set in the past' do
+      let!(:list) { FactoryGirl.build(:list, gift_day: Date.yesterday) }
+
+      it 'should not be valid and give errors' do
+        expect(list).to_not be_valid
+        expect(list.errors[:gift_day]).to be_present
+      end
+    end
+
+    context 'the gift day is set in the future' do
+      let!(:list) { FactoryGirl.build(:list, gift_day: Date.tomorrow) }
+
+      it 'should be valid' do
+        expect(list).to be_valid
+      end
+    end
+
+    context 'the gift day is not set' do
+      let!(:list) { FactoryGirl.build(:list, gift_day: nil) }
+
+      it 'should not be valid and give errors' do
+        expect(list).to_not be_valid
+        expect(list.errors[:gift_day]).to be_present
+      end
+    end
+  end
+
 end
