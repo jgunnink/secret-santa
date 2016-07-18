b8f92e12b7e7af95be9338b1f509b3dc66cc50fc
JK Gunnink <jgunnink@gmail.com>
2016-07-18 23:20:14 +0800

    Adds test cases for feature specs where gift day has passed

--- spec/features/member/lists/member_can_delete_list_spec.rb
+++ spec/features/member/lists/member_can_delete_list_spec.rb
@@ -11,7 +11,6 @@ feature 'Member can delete an existing list' do
   end
 
   scenario "signed in user deletes existing list" do
-
     #ensure list exists before attempting to delete.
     expect(page).to have_content(list.name)
 
@@ -22,6 +21,19 @@ feature 'Member can delete an existing list' do
     expect(page.find('.alert.alert-success')).to have_content("List was successfully deleted")
     expect(page).to_not have_content(list.name)
     expect(page).to have_content("You have no secret santa lists yet!")
+  end
+
+  context "the gift day has passed" do
+    scenario "user cannot delete list" do
+      list.update_attributes(gift_day: Date.yesterday)
+
+      within "table" do
+        click_on("Delete")
+      end
+
+      expect(page.find('.alert.alert-warning')).to have_content("Sorry! As the gift day has passed, you can no longer modify or delete this list!")
+      expect(page).to have_content(list.name)
+    end
   end
 
 end
