39dd6a91fa0845695af449e70918bb14ed939972
JK Gunnink <jgunnink@gmail.com>
2016-07-18 19:50:09 +0800

    Adds dependent: :destroy to user model for lists, updates associated tests

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,5 +5,14 @@ RSpec.describe List do
   describe '@name' do
     it { should validate_presence_of(:name) }
   end
-  
+
+  describe "@santas" do
+    let!(:list) { FactoryGirl.create(:list) }
+    let!(:santa) { FactoryGirl.create(:santa, list_id: list.id) }
+
+    it "deletes associated lists if the user account is deleted" do
+      expect { list.destroy }.to change { Santa.count }.by(-1)
+    end
+  end
+
 end
