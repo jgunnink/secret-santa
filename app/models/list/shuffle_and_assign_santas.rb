64f4d39cba5977ae9f41abe23fda1dee9de83c91
JK Gunnink <jk@thefrontiergroup.com.au>
2016-08-16 13:56:39 +0800

    Update randomiser and assigner

--- app/models/list/shuffle_and_assign_santas.rb
+++ app/models/list/shuffle_and_assign_santas.rb
@@ -7,28 +7,26 @@ class List::ShuffleAndAssignSantas
 
   def assign
     @recipient_list = @santas.to_a
-    @santas.each do |santa|
-      check_validity_of_proposed_recipient(santa)
-    end
+    randomise_and_assign(@recipient_list, @recipient_list, [], [])
   end
 
 private
 
-  def check_validity_of_proposed_recipient(santa)
-    proposed_recipient = @recipient_list.sample
-    if proposed_recipient != santa
-      santa.giving_to = proposed_recipient.id
-      santa.save!
-      @recipient_list.delete(proposed_recipient)
-    else
-      if @recipient_list.count == 1
-        # It can happen that the matcher leaves one person that can only be assigned
-        # to themselves. If this happens, we have to run the whole method again.
-        assign
-      else
-        check_validity_of_proposed_recipient(santa)
-      end
-    end
+  def randomise_and_assign(available_santas, unassigned_santas, assigned_santas, assigned_recipients)
+    return if unassigned_santas.length == 0
+
+    santa = (available_santas - assigned_santas).sample
+    recipient = (available_santas - assigned_recipients - [santa]).sample
+
+    assigned_santas << santa
+    unassigned_santas = available_santas - assigned_santas
+    assigned_recipients << recipient
+
+    santa.giving_to = recipient.id
+    puts "#{santa.name} is giving to #{recipient.name}"
+    santa.save!
+
+    randomise_and_assign(available_santas, unassigned_santas, assigned_santas, assigned_recipients)
   end
 
 end
