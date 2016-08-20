450c0863ce06bd6ad5eca04d0f4301bdaf42132a
JK Gunnink <jk@thefrontiergroup.com.au>
2016-08-20 16:12:11 +0800

    WIP: layout model test

--- spec/models/list/shuffle_and_assign_santas_spec.rb
+++ spec/models/list/shuffle_and_assign_santas_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe List::ShuffleAndAssignSantas do
 
+  describe "#assign_and_email" do
+    subejct { List::ShuffleAndAssignSantas.new(list).assign_and_email }
+
+    # Santas should be assigned a giving_to value
+    # Each santa should recieve an email
+    # No santas should be unassigned
+
+  end
+
 end
