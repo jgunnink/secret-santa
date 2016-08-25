e0ada374887d22e229499ac08c5094ba7ea1a509
JK Gunnink <jgunnink@gmail.com>
2016-08-25 21:15:16 +0800

    Adds inverse_of tests in model specs

--- spec/models/santa_spec.rb
+++ spec/models/santa_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Santa do
     it { should validate_presence_of(:name) }
   end
 
+  describe "@list" do
+    it { should validate_presence_of(:list) }
+    it { should belong_to(:list).inverse_of(:santas) }
+  end
+
   describe "@email" do
     # this record is required for the uniqueness matcher
     let!(:santa) { FactoryGirl.create(:santa) }
