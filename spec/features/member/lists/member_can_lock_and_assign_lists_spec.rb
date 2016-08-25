180eec2e82ef8d824a85c5735c28ccc44c7adbd4
JK Gunnink <jgunnink@gmail.com>
2016-08-25 21:55:29 +0800

    Attempt to resolve more issues from houndCI

--- spec/features/member/lists/member_can_lock_and_assign_lists_spec.rb
+++ spec/features/member/lists/member_can_lock_and_assign_lists_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 feature 'Member can lock and assign santas within a list' do
-
-  let(:list)   { FactoryGirl.create(:list, user_id: user.id) }
-  let(:user)   { FactoryGirl.create(:user, :member) }
-  let!(:santas) { FactoryGirl.create_list(:santa, 5, list_id: list.id)}
+  let(:list)    { FactoryGirl.create(:list, user_id: user.id) }
+  let(:user)    { FactoryGirl.create(:user, :member) }
+  let!(:santas) { FactoryGirl.create_list(:santa, 5, list_id: list.id) }
 
   background do
     sign_in_as(user)
@@ -22,12 +21,11 @@ feature 'Member can lock and assign santas within a list' do
   end
 
   context 'where there are fewer than 3 santas in a list' do
-    let!(:santas) { FactoryGirl.create_list(:santa, 2, list_id: list.id)}
+    let!(:santas) { FactoryGirl.create_list(:santa, 2, list_id: list.id) }
 
     scenario 'the button should be disabled, with information' do
       expect(page).to have_button('Lock, assign and send', disabled: true)
       expect(page).to have_content('Not available if there are less than three (3) Santas in your list')
     end
   end
-
 end
