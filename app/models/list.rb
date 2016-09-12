4502a403f99a0fce0bfc37bc40b88aaa03ee5453
JK Gunnink <jgunnink@gmail.com>
2016-09-12 23:40:20 +0800

    customised form further, added tweaks to email wording, updated form with columns

--- app/models/list.rb
+++ app/models/list.rb
@@ -15,7 +15,7 @@ class List < ActiveRecord::Base
 
   def gift_day_cannot_be_in_the_past
     if gift_day.present? && gift_day < Time.zone.today
-      errors.add(:gift_day, "You can't set the gift day to be in the past")
+      errors.add(:gift_day, "You can't set the gift day to be today or in the past")
     end
   end
 
