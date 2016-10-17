4af79e6e59a07ac7dd9a5054df430da2af6ed4b3
JK Gunnink <jgunnink@gmail.com>
2016-10-18 01:11:11 +0800

    Change default url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # ActionMailer settings
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'staging.secretsanta.website' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
