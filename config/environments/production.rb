e50231b762e1b7346d11cfba8614d215b49066d4
JK Gunnink <jgunnink@gmail.com>
2016-10-18 01:21:29 +0800

    Tweak SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,14 +41,14 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # ActionMailer settings
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'staging.secretsanta.website' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["sendgrid_username"],
     :password => ENV["sendgrid_password"],
-    :domain => 'secretsanta.website',
+    :domain => 'staging.secretsanta.website',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
