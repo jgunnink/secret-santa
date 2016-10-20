5ed1e4bd482de81a2c451091b30096f470381803
JK Gunnink <jgunnink@gmail.com>
2016-10-21 00:32:49 +0800

    Adds extra validations

--- spec/models/santa_spec.rb
+++ spec/models/santa_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Santa do
 
   describe '@name' do
     it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
   end
 
   describe '@list' do
@@ -13,7 +14,7 @@ RSpec.describe Santa do
 
   describe '@email' do
     # this record is required for the uniqueness matcher
-    let!(:santa) { FactoryGirl.create(:santa) }
+    subject { FactoryGirl.build(:santa) }
 
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).scoped_to(:list_id) }
