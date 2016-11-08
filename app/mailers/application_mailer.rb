e2327dd6a8501a5a540913fd6607f0dc8fe74642
JK Gunnink <jgunnink@gmail.com>
2016-11-08 22:55:03 +0800

    Tweak mailer view, and layout

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 # Set defaults for outgoing mail.
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Captain Santa <captain-santa@notifications.secretsanta.website>'
+  default from: 'Captain Santa <captain-santa@secretsanta.website>'
   layout 'mailer'
 end
