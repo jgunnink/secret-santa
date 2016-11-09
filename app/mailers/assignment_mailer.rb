a2155d89d7d2575161cd7eb43e65812bd67c995a
JK Gunnink <jgunnink@users.noreply.github.com>
2016-11-10 01:00:49 +0800

    Hotfix/update embedded image in emails (#48)
    * Apply fix

--- app/mailers/assignment_mailer.rb
+++ app/mailers/assignment_mailer.rb
@@ -4,6 +4,7 @@ class AssignmentMailer < ApplicationMailer
     @santa = santa
     @recipient = find_recipient(@santa)
     @list = @santa.list
+    attachments.inline["santa_with_sack.png"] = File.read("#{Rails.root}/app/assets/images/santa_with_sack_med.png")
     mail(to: @santa.email, subject: "Your Secret Santa for #{@list.name}")
   end
 
