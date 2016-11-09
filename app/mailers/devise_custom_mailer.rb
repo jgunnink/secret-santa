0a5037e1ee4ed5c9127dda8a5199a3fffb6e3ae4
JK Gunnink <jgunnink@gmail.com>
2016-11-10 00:55:11 +0800

    Apply fix

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -4,6 +4,7 @@ class DeviseCustomMailer < Devise::Mailer
   default template_path: "devise/mailer"
 
   def confirmation_instructions(record, token, opts={})
+    attachments.inline["santa_with_sack.png"] = File.read("#{Rails.root}/app/assets/images/santa_with_sack_med.png")
     opts[:from] = "confirmations@secretsanta.website"
     opts[:reply_to] = "confirmations@secretsanta.website"
     super
