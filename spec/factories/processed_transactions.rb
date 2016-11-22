a1f3d47aced6f9cb921fa5e0226e9f56487bb907
JK Gunnink <jk@thefrontiergroup.com.au>
2016-11-22 18:48:26 +0800

    Configure rspec to allow local host connections with webmock

--- spec/factories/processed_transactions.rb
+++ spec/factories/processed_transactions.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :processed_transaction do
     list
-    transaction_id { FFaker::Color.hex_code }
+    transaction_id { ["08156329954HRT119", "12134RDFGRFD119", "08234ERDSDFG312119", "32123RTFRC123419"].sample }
   end
 
 end
