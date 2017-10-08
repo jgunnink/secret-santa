f2cde9fb46ebc666e2551ba50e9de7cafc32a6d1
JK Gunnink <jgunnink@gmail.com>
2017-10-08 15:45:46 +0800

    update letsencrypt listener

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def letsencrypt
-    render text: "6xdgqQj1H-OiTqcHxunCoIFe3PpmQc79mhZ6bt85xhk.kKTy9jb8-EWBH0n43RpJqmx9K_KORWrgALVk_R0gUZo"
+    render text: "ZYFNHyg75KmV-C_yVskOEkbUmqPao2JjzL-EzXRnbwE.VF2Hm6ALAg5PQ9Tv4PL_q1AKXp8xjqbr3ZjHeupdQj8"
   end
 
 end
